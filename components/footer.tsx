0d1e0de69617e967d4005ad5384026aa816208b8
roneygajjar <roneygajjar121@gmail.com>
2026-02-21 11:14:32 +0530

    fix: update footer copyright text to reflect correct company name

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const footer = () => {
     return (
         <footer>
             <div className="mx-auto max-w-7xl px-6 py-10 lg:px-8 flex flex-col md:flex-row justify-between">
-                <p className="text-center text-sm/6 text-gray-500">&copy; 2026 Harshid Engravers, Inc. All rights reserved.</p>
+                <p className="text-center text-sm/6 text-gray-500">&copy; 2026 Wellness & Spa, Inc. All rights reserved.</p>
                 <p className="text-center text-sm/6 text-gray-500"> Design and Developed by HoneyHexa.</p>
             </div>
         </footer>
